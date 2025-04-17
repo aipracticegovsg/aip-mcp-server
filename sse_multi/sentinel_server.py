9451201111fe33325ff772454eb892ba930ef45d
Shangs <ng.shangru@gmail.com>
2025-04-17 11:25:46 +0800

    update sentinel

--- sse_multi/sentinel_server.py
+++ sse_multi/sentinel_server.py
@@ -8,7 +8,7 @@ from starlette.requests import Request
 from starlette.routing import Mount, Route
 from mcp.server import Server
 import uvicorn
-import dotenv
+from dotenv import load_dotenv
 import os
 
 load_dotenv()
